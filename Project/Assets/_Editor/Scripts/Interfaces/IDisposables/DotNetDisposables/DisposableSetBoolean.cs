68b0e26ccc1f9cc1c0dbe6c2f43adb14bdd25024
AkaitoAi <mmubhatti@outlook.com>
2024-10-17 09:38:18 +0500

    Added disposeables

---
+++ Project/Assets/_Editor/Scripts/Interfaces/IDisposables/DotNetDisposables/DisposableSetBoolean.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace AkaitoAi.Extensions
+{
+    public class DisposableSetBoolean : IDisposable
+    {
+        private readonly Action<bool> _action;
+        private readonly bool _startingValue;
+
+        public DisposableSetBoolean(Action<bool> action, bool startingValue)
+        {
+            _action = action;
+            _startingValue = startingValue;
+            _action?.Invoke(startingValue);
+        }
+
+        public void Dispose()
+        {
+            _action?.Invoke(!_startingValue);
+        }
+    }
+}
