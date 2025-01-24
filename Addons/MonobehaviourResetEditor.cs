08c6cf327a841b0b3887f980a8fc6f6786cd08a6
AkaitoAi <102175025+AkaitoAi@users.noreply.github.com>
2025-01-24 15:04:45 +0500

    some random scripts

---
+++ Addons/MonobehaviourResetEditor.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(MonoBehaviour), true)]
+public class MonobehaviourResetEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Draw the default inspector
+        DrawDefaultInspector();
+
+        // Check if the target ScriptableObject implements IResettable
+        if (target is IResettable resettable)
+        {
+            // Add a "Reset to Default Values" button
+            if (GUILayout.Button("Reset to Default Values"))
+            {
+                resettable.ResetToDefaults();
+                EditorUtility.SetDirty(target); // Mark the object as dirty for changes to persist
+                Debug.Log($"{target.name} reset to default values.");
+            }
+        }
+    }
+}
+#endif
