20447a028d721c17ebd0b3dcd6a8eb2ba04f4bf4
AkaitoAi <102175025+AkaitoAi@users.noreply.github.com>
2025-03-11 11:35:22 +0500

    Update RigidbodyColliderToggler.cs

--- Project/Assets/_Editor/Scripts/RigidbodyColliderToggler.cs
+++ Project/Assets/_Editor/Scripts/RigidbodyColliderToggler.cs
@@ -18,22 +18,56 @@ namespace AkaitoAi
 
         public void ToggleRigidbodyKinematic()
         {
-            // Flip the state first
             kinematicState = !kinematicState;
             UpdateRigidbodyStates();
 #if UNITY_EDITOR
-            UnityEditor.EditorUtility.SetDirty(this);
+        UnityEditor.EditorUtility.SetDirty(this);
 #endif
         }
 
         public void ToggleColliders()
         {
-            // Flip the state first
             colliderState = !colliderState;
             UpdateColliderStates();
 #if UNITY_EDITOR
-            UnityEditor.EditorUtility.SetDirty(this);
+        UnityEditor.EditorUtility.SetDirty(this);
 #endif
+        }
+
+        public void RemoveJoints()
+        {
+            Joint[] joints = GetComponentsInChildren<Joint>();
+            foreach (Joint joint in joints)
+            {
+                if (Application.isEditor)
+                {
+                    DestroyImmediate(joint);
+                }
+            }
+        }
+
+        public void RemoveColliders()
+        {
+            Collider[] colliders = GetComponentsInChildren<Collider>();
+            foreach (Collider col in colliders)
+            {
+                if (Application.isEditor)
+                {
+                    DestroyImmediate(col);
+                }
+            }
+        }
+
+        public void RemoveRigidbodies()
+        {
+            Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody rb in rigidbodies)
+            {
+                if (Application.isEditor)
+                {
+                    DestroyImmediate(rb);
+                }
+            }
         }
 
         private void UpdateRigidbodyStates()
@@ -55,26 +89,58 @@ namespace AkaitoAi
         }
 
 #if UNITY_EDITOR
-        [CustomEditor(typeof(RigidbodyColliderToggler))]
-        public class RigidbodyColliderTogglerEditor : Editor
+    [CustomEditor(typeof(RigidbodyColliderToggler))]
+    public class RigidbodyColliderTogglerEditor : Editor
+    {
+        public override void OnInspectorGUI()
         {
-            public override void OnInspectorGUI()
+            DrawDefaultInspector();
+
+            RigidbodyColliderToggler toggler = (RigidbodyColliderToggler)target;
+
+            if (GUILayout.Button("Toggle Rigidbody Kinematic"))
             {
-                DrawDefaultInspector();
+                toggler.ToggleRigidbodyKinematic();
+            }
 
-                RigidbodyColliderToggler toggler = (RigidbodyColliderToggler)target;
+            if (GUILayout.Button("Toggle Colliders"))
+            {
+                toggler.ToggleColliders();
+            }
 
-                if (GUILayout.Button("Toggle Rigidbody Kinematic"))
+            GUILayout.Space(10); // Add some spacing
+
+            if (GUILayout.Button("Remove All Joints"))
+            {
+                if (EditorUtility.DisplayDialog("Confirm Removal",
+                    "Are you sure you want to remove all joints from children?",
+                    "Yes", "No"))
                 {
-                    toggler.ToggleRigidbodyKinematic();
+                    toggler.RemoveJoints();
                 }
+            }
 
-                if (GUILayout.Button("Toggle Colliders"))
+            if (GUILayout.Button("Remove All Colliders"))
+            {
+                if (EditorUtility.DisplayDialog("Confirm Removal",
+                    "Are you sure you want to remove all colliders from children?",
+                    "Yes", "No"))
                 {
-                    toggler.ToggleColliders();
+                    toggler.RemoveColliders();
+                }
+            }
+
+            if (GUILayout.Button("Remove All Rigidbodies"))
+            {
+                if (EditorUtility.DisplayDialog("Confirm Removal",
+                    "Are you sure you want to remove all rigidbodies from children?",
+                    "Yes", "No"))
+                {
+                    toggler.RemoveRigidbodies();
                 }
             }
         }
+    }
 #endif
     }
 }
