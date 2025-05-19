d9ca8851a2cb528a74661afd416742bf12bd3c8e
AkaitoAi <102175025+AkaitoAi@users.noreply.github.com>
2025-05-19 08:49:37 +0500

    added wrap/unwrap angles

--- Project/Assets/_Editor/Scripts/AkaitoAiExtensions.cs
+++ Project/Assets/_Editor/Scripts/AkaitoAiExtensions.cs
@@ -695,6 +695,25 @@ namespace AkaitoAi.Extensions
             return (dot > forwardAngles.x && dot < forwardAngles.y) || (dot > backwardAngles.x && dot < backwardAngles.y)? true : false;
         }
 
+        public static float WrapAngle(this float angle)
+        {
+            angle %= 360;
+            if (angle > 180)
+                return angle - 360;
+
+            return angle;
+        }
+
+        public static float UnwrapAngle(this float angle)
+        {
+            if (angle >= 0)
+                return angle;
+
+            angle = -angle % 360;
+
+            return 360 - angle;
+        }
+
         #endregion
 
         #region Rigidbody
